4ed823bf8b70b14d31af315637e52fbe46a81923
Alex Bajaña <58230734+AlexB4891@users.noreply.github.com>
2020-07-09 17:10:27 -0500

    Script de la segunda clase

---
+++ 01_ayuda_recursos/01_script.R
@@ -0,0 +1,27 @@
+
+# Clase 1: Busqueda de ayuda ----------------------------------------------
+
+# Añadir sección; ctrl + shift + R
+# Comentarios: numeral (#)
+# Comentar una linea de código; ctrl + shift + C
+# Ejecutar; ctrl + enter
+# Guardar: ctrl + S
+
+# Buscar ayuda:
+
+## Abrir ventana de ayuda:
+
+help.start()
+
+## Buscar ayuda por tema:
+
+help.search("normal distribution")
+
+## Buscar ayuda sobre una función:
+
+help(rnorm)
+
+# Clase 1: Environments ---------------------------------------------------
+
+
+.packages()
