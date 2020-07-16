49e02e569c504c20ca288bb3ef73db3540240c49
Alex Bajaña <58230734+AlexB4891@users.noreply.github.com>
2020-07-16 16:25:41 -0500

    Material clase 5

---
+++ 05_estrucuturas_control/clase_5_fibonacci.R
@@ -0,0 +1,20 @@
+############################################################################
+#                  CURSO: Herramientas de análisis con R                   #
+#                     CLASE 3: Estructuras de control                      #
+############################################################################
+
+# Una serie Fibonacci es una secuencia de números donde el valor i es igual 
+# a la suma de sus dos predecesores:
+# 
+# Ejemplo:  0 1 1 2 3 5 8 13 21
+# 
+# Ejercicio: generar una serie de Fibonacci y ubicar el elemento 300 de esta
+# serie
+
+# Alternativa 1: A partir de un vector vacio generamos la serie, como los 
+# tres primeros números de la serie son fijos creo un condicional para las 
+# posiciones 1 (i = 3), 2 (i = 4), 3 (i = 5) para valores i >= 6 ya aplico 
+# la suma de los dos antesores
+
+
+
