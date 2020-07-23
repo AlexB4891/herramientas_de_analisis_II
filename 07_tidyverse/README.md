716d2ac52fe5fc742d8fd27d829127a79a65d4e6
Alex Bajaña <alexb.inf.4891@gmail.com>
2020-07-23 18:55:03 -0500

    Presentación completa

---
+++ 07_tidyverse/README.md
@@ -0,0 +1,16 @@
+# Curso Herramientas de análisis con R
+
+En este repositorio vas a encontrar el material de la clase 1 del curso en mención:
+
+## Un paquete para gobernarlos a todos
+
+La presentación se encuentra en el link:
+
+https://alexvbr22082018.github.io/R_tools_with_R_class_5/
+
+No dudes en dejar tu comentario o sugerencia.
+
+### Contenido
+
+- La presentación se realiza a partir del archivo index.Rmd
+- El archivo **saiku-export.csv** contiene la consulta realizada en el sistema SAIKU del SRI y que se muestra en la presentación
--- 07_tidyverse/notas_clase_7.R
+++ 07_tidyverse/notas_clase_7.R
@@ -59,3 +59,83 @@ select(tabla,matches("^.{5}$"))
 # Una de:
 select(tabla,one_of(c("Anio","año","anio")))
 
+ind <- sample(1:nrow(tabla),30)
+
+tabla$ventas_t[ind] <- NA_real_
+
+filter(tabla,is.na(ventas_t))
+# Negación:
+filter(tabla,!is.na(ventas_t))
+
+
+# Con textos:
+# ^S siginifa "comienza con"
+
+ind <- grepl(x = tabla$provin,pattern = "^S")
+
+f_tabla <- filter(tabla,ind) 
+
+table(f_tabla$provin)
+
+
+# Varias condiciones:
+
+filter(tabla, 
+       activ == "A", 
+       anio == 2016) 
+
+# Uso del "o |"
+filter(tabla, 
+       provin == "CHIMBORAZO",
+       mes == 4 | mes == 5, 
+       anio == 2016,
+       ventas_t >= 20e6) 
+
+# Uso del "%in%"
+filter(tabla, provin == "CHIMBORAZO",
+       mes %in% c(4, 5),
+       anio == 2016,
+       ventas_t >= 20e6)
+
+# Prueba 2: ---------------------------------------------------------------
+
+prueba_2 <- tabla %>% filter(estado == "ACTIVO",
+                 grupo_e == "SI",
+                 mes %in% c(1:6),
+                 anio == 2015)
+
+
+
+min(prueba_2$ventas_t)
+
+max(prueba_2$ventas_t)
+
+# Orden 
+prueba_2 %>% 
+  arrange(ventas_t)
+
+prueba_2 %>% 
+  arrange(desc(compras_t))
+
+
+# Yo quiero un ranking de ventas de aquellos provincias que tengan compras
+# mayores a 5e6 en el 2016
+
+tabla %>% 
+  select(anio,mes,provin,ventas_t,compras_t) %>% 
+  filter(compras_t > 500e6,
+         anio == 2016) %>% 
+  arrange(desc(ventas_t))
+
+
+# Justificacion del uso de la pipe:
+
+# select(filter(arrange(tabla,desc(ventas_t)),compras_t > 500e6,
+#        anio == 2016))
+
+tabla1 <- select(tabla,anio,mes,provin,ventas_t,compras_t)
+
+tabla2 <- filter(tabla1,compras_t > 500e6,
+                 anio == 2016)
+
+tabla3 <- arrange(tabla2,desc(ventas_t))
